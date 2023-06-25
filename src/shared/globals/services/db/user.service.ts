7000318566247abd74e9a512c25245984317f821
Marcelo-G21 <marcelo.garcia.arenas@gmail.com>
2023-06-25 17:42:51 -0400

    added auth and user database service

---
+++ src/shared/globals/services/db/user.service.ts
@@ -0,0 +1,33 @@
+import { IUserDocument } from '@user/interfaces/userDocument.interface';
+import { UserModel } from '@user/models/user.schema';
+import mongoose from 'mongoose';
+
+class UserService {
+  public async addUserData(data: IUserDocument): Promise<void> {
+    await UserModel.create(data);
+  }
+
+  public async getUserById(userId: string): Promise<IUserDocument> {
+    const users: IUserDocument[] = await UserModel.aggregate([
+      { $match: { _id: new mongoose.Types.ObjectId(userId) } },
+      { $lookup: { from: 'Auth', localField: 'authId', foreignField: '_id', as: 'authId' } },
+      { $unwind: '$authId' },
+      { $project: this.aggregateProject() },
+    ]);
+    return users[0];
+  }
+
+  private aggregateProject() {
+    return {
+      _id: 1,
+      username: '$authId.username',
+      uId: '$authId.uId',
+      email: '$authId.email',
+      createdAt: '$authId.createdAt',
+      petsCount: 1,
+      profilePicture: 1
+    };
+  }
+}
+
+export const userService: UserService = new UserService();

7d45b6b57ffeae69ea6231f0d401cf16d562a311
Marcelo-G21 <marcelo.garcia.arenas@gmail.com>
2023-06-24 23:09:29 -0400

    auth schemes added

--- src/features/auth/schemes/signin.ts
+++ src/features/auth/schemes/signin.ts
@@ -1,6 +1,6 @@
 import Joi, { ObjectSchema } from 'joi';
 
-const signupSchema: ObjectSchema = Joi.object().keys({
+const loginSchema: ObjectSchema = Joi.object().keys({
   username: Joi.string().required().min(4).max(8).messages({
     'string.base': 'Username must be of type string',
     'string.min': 'Invalid username',
@@ -12,18 +12,7 @@ const signupSchema: ObjectSchema = Joi.object().keys({
     'string.min': 'Invalid password',
     'string.max': 'Invalid password',
     'string.empty': 'Password is a required field'
-  }),
-  email: Joi.string().required().email().messages({
-    'string.base': 'Email must be of type string',
-    'string.email': 'Email must be valid',
-    'string.empty': 'Email is a required field'
-  }),
-  avatarColor: Joi.string().required().messages({
-    'any.required': 'Avatar color is required'
-  }),
-  avatarImage: Joi.string().required().messages({
-    'any.required': 'Avatar image is required'
   })
 });
 
-export { signupSchema };
+export { loginSchema };

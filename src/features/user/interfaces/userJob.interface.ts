03d7ba31f27694bc3b60cd630f065cf084ecda33
Marcelo-G21 <marcelo.garcia.arenas@gmail.com>
2023-06-26 04:33:56 -0400

    auth and user workers, email and user job interfaces, base queue

---
+++ src/features/user/interfaces/userJob.interface.ts
@@ -0,0 +1,8 @@
+import { IUserDocument } from './userDocument.interface';
+
+export interface IUserJob {
+  keyOne?: string;
+  keyTwo?: string;
+  key?: string;
+  value?: string | IUserDocument;
+}
---
+++ src/shared/globals/workers/user.worker.ts
@@ -0,0 +1,22 @@
+import { DoneCallback, Job } from 'bull';
+import Logger from 'bunyan';
+import { logger } from '@configs/configLogs';
+import { userService } from '@services/db/user.service';
+
+const log: Logger = logger.createLogger('userWorker');
+
+class UserWorker {
+  async addUserToDB(job: Job, done: DoneCallback): Promise<void> {
+    try {
+      const { value } = job.data;
+      await userService.addUserData(value);
+      job.progress(100);
+      done(null, job.data);
+    } catch (error) {
+      log.error(error);
+      done(error as Error);
+    }
+  }
+}
+
+export const userWorker: UserWorker = new UserWorker();
